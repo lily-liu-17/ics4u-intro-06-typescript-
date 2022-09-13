e2d4a12659d6a11575a8c148371e16680f94983a
lily-liu-17 <lily.liu@mths.ca>
2022-09-13 01:05:28 +0000

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * @author  Lily Liu
+ * @version 1.0
+ * @since   2022-09-12
+ */
+
+console.log('Hello, World!')
+
+console.log('\nDone')
